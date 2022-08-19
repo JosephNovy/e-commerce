dc13ae0cbf69721030923a499a5c9e25ba02cc01
Joseph Novy Quadri <jnq1992@gmail.com>
2022-08-19 09:14:11 -0300

    att

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,6 @@
 // domain/.netlify/function/hello
 
-exports.handle = async function (event, context) {
+exports.handler = async function (event, context) {
   return {
     statusCode: 200,
     body: "hello wolrd",
